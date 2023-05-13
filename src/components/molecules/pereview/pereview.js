3c9fe1289e731a2750d0ed5822536887006087d6
Alibn22 <a.baghchehban1@gmail.com>
2023-05-13 11:10:13 +0430

    add pereview in template

--- src/components/molecules/pereview/pereview.js
+++ src/components/molecules/pereview/pereview.js
@@ -1,12 +1,17 @@
 import PerviewComponent from '../../atoms/pereview/pereview'
 import Button from '../../atoms/button/button'
+import ProgressBar from '../../atoms/progressBar/progressBar'
 
-const Webcam = ({perview,button}) => {
+const Webcam = ({perview,button,videoSize,upload,percent}) => {
 
     return (
         <div style={{display:'flex',flexDirection:'column'}}>
             <PerviewComponent src={perview.src} />
-            <Button name={button.name} title={button.title}  {...button}/>
+            <span style={{marginTop:'20px'}}>حجم : {videoSize} مگابایت</span>
+            {!upload?
+                <Button name={button.name} title={button.title}  {...button}/>:
+                <ProgressBar completed={percent}/>
+            }
         </div>
     )
 }
--- src/components/templates/webcam/template.js
+++ src/components/templates/webcam/template.js
@@ -1,11 +1,14 @@
 import Webcam from '../../molecules/webcam/webcam'
-
+import Pereview from '../../molecules/pereview/pereview'
 
 const Template = (props) => {
 
     return (
         <div style={{width:'100%',minHeight:'100vh',display:'flex',justifyContent:'center',alignItems:'center'}}>
-            <Webcam button={props.button} webcam={props.webcam} />
+            {!props.capturing?
+            <Webcam button={props.button[0]} webcam={props.webcam} />:
+            <Pereview perview={props.pereview} upload={props.upload} percent={props.percent} button={props.button[1]} videoSize={props.videoSize} />
+            }
         </div>
     )
 }
