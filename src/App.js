3c606b43e65521dad4935ce66ddd8aa1042596c5
Alibn22 <a.baghchehban1@gmail.com>
2023-05-20 08:55:57 +0330

    add secure origin to fix bug mobile device error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+// import { useEffect } from 'react';
 import './App.css';
 import WebcamRecorder from './components/views/webcamRecorder/webcamRecorder';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+function App () {
+  // useEffect(() => {
+  //   debugger
+  //   const timeout = setTimeout(() => {
+  //     debugger
+  //     // 👇️ redirects to an external URL
+  //     if (window.location.protocol === 'http:') {
+  
+  //       window.location.href =
+  //         window.location.href.replace(
+  //           'http:', 'https:');
+  //     }
+  //   }, 3000);
+  
+  //   return () => clearTimeout(timeout);
+  // }, [])
   return (
     <div className="App">
       <WebcamRecorder/>
